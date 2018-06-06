472556bc02f3bc9582e157b0c0c79bf11940707b
germanescudero <32752444+germanescudero@users.noreply.github.com>
2018-06-06 16:24:47 -0300

    agregando nuevo formulario al practico 3

---
+++ practico4/validacion.js
@@ -0,0 +1,56 @@
+function validarFormulario(f) {
+                    
+    if (f.nombre.value == "") {
+    document.getElementById("label").style.color = "red";
+    document.getElementById("label").innerHTML = "* Campo Obligatorio"; 
+    return false;
+    }
+     if (f.apellido.value == "") {
+        document.getElementById("label2").style.color = "red";
+        document.getElementById("label2").innerHTML = "* Campo Obligatorio";
+
+        return false;
+         }
+    
+    if (f.telefono.value == "") {
+        document.getElementById("label3").style.color = "red";
+        document.getElementById("label3").innerHTML="* Campo Obligatorio";
+       
+        return false;
+    }
+    if (f.email.value == "") {
+        document.getElementById("label4").style.color = "red";
+        document.getElementById("label4").innerHTML = "* Campo Obligatorio";
+       
+        return false;
+    }
+  
+alert("CAMPOS CORRECTAMENTE SUMINISTRADOS ");
+return true;
+}
+
+function limpiarLabel(){
+document.getElementById("label").innerHTML = "";
+document.getElementById("label2").innerHTML = "";
+document.getElementById("label3").innerHTML = "";
+document.getElementById("label4").innerHTML = "";
+
+}
+
+function iniciarLabel(){
+    document.getElementById("label").innerHTML = "* Campo Obligatorio";
+    document.getElementById("label2").innerHTML = "* Campo Obligatorio";
+    document.getElementById("label3").innerHTML = "* Campo Obligatorio";
+    document.getElementById("label4").innerHTML = "* Campo Obligatorio";
+    
+    }
+
+function validacionEmail(valor) {
+   // var valor = document.getElementById("email").value;
+    if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(valor))) {
+        document.getElementById("label4").innerHTML="Email Incorrecto";
+        return false;
+    }
+    
+    return true;
+} 
